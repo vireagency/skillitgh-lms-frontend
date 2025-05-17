76ded282d00acb33c2f72c09f57a4376a4f0f97a
King1081 <papahquansah@gmail.com>
2025-05-17 10:35:23 +0000

    style: Update backdrop blur effect in WorkshopsPage for improved visual consistency

--- src/pages/WorkshopsPage.jsx
+++ src/pages/WorkshopsPage.jsx
@@ -274,7 +274,7 @@ const WorkshopsPage = () => {
             />);
           })}
         {selectedWorkshop && view === "upcoming" && (
-          <div className="fixed inset-0 bg-black backdrop-blur-sm flex items-center justify-center z-50">
+          <div className="fixed inset-0 backdrop-blur-[1px] flex items-center justify-center z-50">
             <div className="bg-white rounded-3xl border-2 border-green-500 py-10 px-20 w-[90%] max-w-lg relative">
               <button
                 className="absolute top-2 left-4 text-3xl text-red-500 hover:text-gray-800"
@@ -339,7 +339,7 @@ const WorkshopsPage = () => {
         )}
 
         {showSuccessModal &&  (
-          <div className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50">
+          <div className="fixed inset-0 backdrop-blur-[1px] flex items-center justify-center z-50">
             <div className="bg-white rounded-3xl border-2 border-green-500 p-6 w-[90%] max-w-lg h-[50%] relative text-center">
               <button
                 className="absolute top-3 left-4 text-4xl text-red-500 hover:text-gray-800"
