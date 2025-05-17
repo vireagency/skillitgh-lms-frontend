2a0828dd743bcd544da062a23fad7d4cdbeae2a4
King1081 <papahquansah@gmail.com>
2025-05-17 10:35:15 +0000

    style: Update modal styles in CoursesDashboard for improved aesthetics

--- src/pages/CoursesDashboard.jsx
+++ src/pages/CoursesDashboard.jsx
@@ -135,7 +135,7 @@ const CoursesDashboard = () => {
             />
           ))}
           {selectedCourse && view === "other" && (
-            <div className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50">
+            <div className="fixed inset-0 backdrop-blur-[1px] flex items-center justify-center z-50">
             <div className="bg-white rounded-3xl border-2 border-green-500 py-10 px-20 w-[90%] max-w-lg relative">
               <button
                 className="absolute top-2 left-4 text-3xl text-red-500 hover:text-gray-800"
@@ -151,7 +151,7 @@ const CoursesDashboard = () => {
               <h2 className="text-xl font-semibold mb-2">{selectedCourse.title}</h2>
               <p className="text-sm text-gray-700 mb-4">{selectedCourse.description}</p>
               <button
-                className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+                className="bg-green-500 text-white px-4 py-2 rounded-3xl hover:bg-green-600"
                 onClick={() => {
                   axios
                     .post(
@@ -190,10 +190,10 @@ const CoursesDashboard = () => {
           </div>
         )}
         {showSuccessModal && (
-          <div className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50">
+          <div className="fixed inset-0 backdrop-blur-[1px] flex items-center justify-center z-50">
             <div className="bg-white rounded-3xl p-6 w-[90%] max-w-lg h-[50%] relative text-center">
               <button
-                className="absolute top-3 left-4 text-8xl text-red-500 hover:text-gray-800"
+                className="absolute top-3 left-4 text-4xl text-red-500 hover:text-gray-800"
                 onClick={() => setShowSuccessModal(false)}
               >
                 &times;
